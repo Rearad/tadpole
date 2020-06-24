13f042f9a34a058aec09809c8dd5a2df1df0c88f
Dizzy <windowsphone520t@live.com>
2020-06-24 17:11:50 +0800

    1：修改启动端口

--- main.go
+++ main.go
@@ -237,7 +237,7 @@ func main() {
 	fmt.Println("Starting application...")
 	go manager.start()
 	http.HandleFunc("/ws", wsPage)
-	http.ListenAndServe(":12345", nil)
+	http.ListenAndServe(":2508", nil)
 }
 
 func wsPage(res http.ResponseWriter, req *http.Request) {
